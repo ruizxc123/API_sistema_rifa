927083af0afdfe6b0d6f25eba601e488c889e04a
Ruizxc123 <ruizxc123@gmail.com>
2026-04-10 16:52:03 -0300

    teste 1

--- frontend/js/config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    API_URL: 'http://localhost:5000/api',
-    TIMEOUT_MINUTES: 5
-};
--- validargmail.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def validar_email(email):
-    padrao = r'^[\w\.-]+@gmail\.com$'
-    return re.match(padrao, email) is not None
-
-# Teste
-print(validar_email("teste@gmail.com"))  # True
-print(validar_email("teste@outlook.com"))  # False

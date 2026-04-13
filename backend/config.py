9e6a03256a0e5c75241d9cb5eb266b8cb9f2ac75
Rui Ferreira Cabral Medeiros Magalhães <167259567+ruizxc123@users.noreply.github.com>
2026-04-13 16:11:04 -0300

    Atualizar o config.py

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'rifa-secret-key')
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'cliente)
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
     MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'rifa_online')
